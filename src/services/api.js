663bbd6ba77759059521dd30dbd15451c294bf4a
stamatispap558 <stampap1997@gmail.com>
2025-06-14 16:36:17 +0300

    added api.js

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+// src/services/api.js
+import axios from 'axios';
+
+const API_URL = 'https://mockapi.wellics.cloud/api';
+
+export async function fetchUser() {
+  const res = await axios.get(`${API_URL}/user`);
+  return res.data;
+}
+
+export async function fetchMetrics(type, duration) {
+  const res = await axios.get(`${API_URL}/metrics`, {
+    params: { type, duration },
+  });
+  return res.data;
+}
+
+export async function fetchTip() {
+  const res = await axios.get(`${API_URL}/tip`);
+  console.log(res.data)
+  return res.data;
+}
